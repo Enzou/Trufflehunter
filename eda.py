bc5a4dfe2040ff9d27ff01f69ed7714a7f964e9d
markus_gierlinger <s1510239009@students.fh-hagenberg.at>
2020-01-12 22:53:36 +0100

    made pruning of tree more interactive

--- eda.py
+++ eda.py
@@ -1,14 +1,16 @@
-from typing import Tuple
+import functools
+from typing import Tuple, Optional, Callable, Dict
 from pathlib import PurePath
-from anytree import Node, RenderTree, ContRoundStyle, Resolver, ChildResolverError
-from anytree.exporter import DotExporter
+
+import graphviz
+from anytree import Node, RenderTree, ContRoundStyle, Resolver, ChildResolverError, PostOrderIter
+from anytree.exporter import DotExporter, DictExporter
 import streamlit as st
 import pandas as pd
-import tqdm
+from anytree.importer import DictImporter
+from tqdm import tqdm
 
-from src.utils.io import load_csv_data
-
-FILE_NAME = 'dt_sessions_full.csv'
+import src.utils.io as io
 
 mappings = [
     "author",  # under blog
@@ -43,6 +45,9 @@ mappings = [
 ]
 
 
+# event_information: ['dynatrace-go', 'perform*' ]
+
+
 # def get_node(root: Node, path: List) -> Node:
 #     node = root
 #     for f in path:
@@ -50,11 +55,30 @@ mappings = [
 #     return node
 #
 
-def build_page_hierarchy(df: pd.DataFrame) -> Tuple[Node, int]:
+
+class tqdm:
+    def __init__(self, iterable, title=None):
+        if title:
+            st.write(title)
+        self.prog_bar = st.progress(0)
+        self.iterable = iterable
+        self.length = len(iterable)
+        self.i = 0
+
+    def __iter__(self):
+        for obj in self.iterable:
+            yield obj
+            self.i += 1
+            current_prog = self.i / self.length
+            self.prog_bar.progress(current_prog)
+
+
+@st.cache
+def build_page_hierarchy(df: pd.DataFrame, update_fn: Callable = lambda x: x) -> Tuple[Dict, int]:
     root = Node('/', count=0)
     tree = {'/': root}
     max_count = 0  # for the color
-    for ua_name in tqdm.tqdm(df.ua_name):
+    for ua_name in update_fn(df.ua_name):
         if "loading of page" in ua_name:
             url_path = ua_name.split(' ')[-1]
             frags = url_path.split("/")[1:-1]
@@ -64,7 +88,9 @@ def build_page_hierarchy(df: pd.DataFrame) -> Tuple[Node, int]:
             for i, (parent, node) in enumerate(zip(frags[:-1], frags[1:])):  # add missing in between nodes
                 id = '/'.join([parent, node])
                 if id not in tree:
-                    if parent in mappings and node != 'author':
+                    # pre-pruning:
+                    # page indicates pagination of previous site, so just repeat it
+                    if (parent in mappings and node != 'author') or node == "page":
                         drop_last_n = len(frags) - i - 1
                         frags = frags[:-drop_last_n]  # drop resource for aggregation # TODO fix dirty hack for pruned tree
                         break
@@ -79,42 +105,45 @@ def build_page_hierarchy(df: pd.DataFrame) -> Tuple[Node, int]:
             if node.count > max_count:
                 max_count = node.count
 
-    return tree['/'], max_count
+    exporter = DictExporter()
+    d = exporter.export(root)
+
+    return d, max_count
 
 
-def build_page_hierarchy_slow(df: pd.DataFrame) -> Tuple[Node, int]:
-    root = Node('.', count=0)
-    r = Resolver("name")
-    max_count = 0  # for the color
+# def build_page_hierarchy_slow(df: pd.DataFrame) -> Tuple[Node, int]:
+#     root = Node('.', count=0)
+#     r = Resolver("name")
+#     max_count = 0  # for the color
+#
+#     for ua_name in tqdm(df.ua_name):
+#         if "loading of page" in ua_name:
+#             p = PurePath('.' + ua_name.split(' ')[-1])
+#
+#             try:
+#                 # parent = r.get(root, str(p.parent))
+#                 path = str(p)
+#                 node = r.get(root, str(p))
+#             except ChildResolverError:
+#                 parent = root
+#                 # recursively create parent nows if missing
+#                 for par in list(p.parents)[1::-1]:  # first two items are '.' and root node -> skip them
+#                     try:
+#                         parent = r.get(root, str(p))
+#                     except ChildResolverError:
+#                         parent = Node(par.name, parent, count=0)
+#                 node = Node(p.name, parent, count=0)
+#
+#             node.count += 1
+#             if node.count > max_count:
+#                 max_count = node.count
+#
+#     return root, max_count
 
-    for ua_name in tqdm.tqdm(df.ua_name):
-        if "loading of page" in ua_name:
-            p = PurePath('.' + ua_name.split(' ')[-1])
 
-            try:
-                # parent = r.get(root, str(p.parent))
-                path = str(p)
-                node = r.get(root, str(p))
-            except ChildResolverError:
-                parent = root
-                # recursively create parent nows if missing
-                for par in list(p.parents)[1::-1]:  # first two items are '.' and root node -> skip them
-                    try:
-                        parent = r.get(root, str(p))
-                    except ChildResolverError:
-                        parent = Node(par.name, parent, count=0)
-                node = Node(p.name, parent, count=0)
-
-            node.count += 1
-            if node.count > max_count:
-                max_count = node.count
-
-    return root, max_count
-
-
-def build_page_visit_graph(df: pd.DataFrame) -> None:
+def build_page_visit_graph(df: pd.DataFrame) -> Node:
     tree = {'/': Node('/')}
-    for ua_name in tqdm.tqdm(df.ua_name):
+    for ua_name in tqdm(df.ua_name):
         if "loading of page" in ua_name:
             url_path = ua_name.split(' ')[-1]
             frags = url_path.split("/")[:-1]
@@ -126,56 +155,86 @@ def build_page_visit_graph(df: pd.DataFrame) -> None:
             for parent, node in zip(frags[:-1], frags[1:]):  # add missing in between nodes
                 tree[node] = Node(node, tree[parent])
 
-    fig = DotExporter(tree['/'])
-    with open(FILE_NAME.replace('.csv', '.dot'), 'w') as f:
-        for l in fig:
-            f.write(f"{l}\n")
+    return tree['/']
 
 
-def is_leaf_parent(node: Node) -> bool:
-    pass
+def prune_tree(root: Node, min_nodes: int = 2) -> Node:
+    """
+    Post-Prune page tree after creation by removing nodes that occured at most 1 time.
+    Furthermore, intermediary nodes, that never have actually been visited and have only one child will be collapsed
+    """
+    for node in PostOrderIter(root):
+        if node.count < min_nodes:
+            node.parent = None
+            del node
+
+    return root
 
 
-def prune_tree(node: Node, max_count: int) -> DotExporter:
-    pass
-
-
-def analyze_paths(df: pd.DataFrame) -> None:
-    # start = timeit()
-    # root, max_count = build_page_hierarchy(df)
-    # mid = timeit()
-    root, max_count = build_page_hierarchy(df)
-    # end = timeit()
-
-    # print(f"Timings: new: {mid-start} / old: {end - mid}")
+def analyze_paths(df: pd.DataFrame) -> Tuple[Node, int]:
+    tree, max_count = build_page_hierarchy(df)
+    root = DictImporter().import_(tree)
 
     # TODO filter sessions with only 1 entry
-    # TODO filter nodes in tree with at most 1 visit
 
     if st.checkbox("Show pagetree in ASCII?", value=False):
         st.text(RenderTree(root, style=ContRoundStyle()))
     if st.checkbox("Show pagetree as graph?", value=True):
         st.text(f"max count {max_count}")
         # color is HSV; saturation determines color intensity
-        fig = DotExporter(root, options=["node [color=black, style=filled];"],
-                          nodenamefunc=lambda n: f"{n.name}\ncount={n.count}",
-                          nodeattrfunc=lambda node: f'fillcolor="0.6 {node.count / max_count} 1"')
 
-        if st.checkbox("Prune?"):
-            prune_tree(root, max_count)
+        if st.checkbox("Prune?", value=True):
+            print("Pruning tree")
+            min_counts = st.slider("Minimum number of counts:", value=2)
+            root = prune_tree(root, min_counts)
 
-        img_name = FILE_NAME.replace('.csv', '.svg')
-        fig.to_picture(img_name)
-        print(f"exported page tree image to '{img_name}'")
-        st.graphviz_chart(''.join(fig))
-        # for l in fig:
-        #     print(l)
+        no_leafs = len([True for n in PostOrderIter(root) if n.is_leaf])
+        print(f"Tree has {no_leafs} leaf-nodes")
+
+    return root, max_count
+
+
+def draw_page_tree(root: Node, max_count: int) -> DotExporter:
+    fig = DotExporter(root, options=["rankdir = LR; node [color=black, style=filled];"],
+                      nodenamefunc=lambda n: f"{n.name}\ncount={n.count}",
+                      nodeattrfunc=lambda node: f'fillcolor="0.6 {node.count / max_count} 1"')
+
+    st.graphviz_chart(''.join(fig))
+    # for l in fig:
+    #     print(l)
+    return fig
+
+
+def export_page_tree(fig: DotExporter, file_name: str) -> None:
+    img_name = file_name.replace('.csv', '.svg')
+    fig.to_picture(img_name)
+    print(f"exported page tree image to '{img_name}'")
+
+
+def load_data(file_name: str) -> pd.DataFrame:
+    st.spinner("Loading data " + file_name)
+    df = io.load_csv_data(file_name)
+    st.write(f"Loaded {len(df)} entries")
+    return df
 
 
 def main():
+    if not st._is_running_with_streamlit:
+        print("Not using streamlit - using fallback instead")
     st.title("Exploratory Data Analysis")
-    df = load_csv_data(FILE_NAME)
-    analyze_paths(df)
+
+    st.sidebar.radio("Stage:", ['Exploratory Data Analysis'])
+
+    available_files = io.get_available_datasets()
+    file_name = st.sidebar.selectbox("Source web log: ",
+                                     options=available_files,
+                                     index=available_files.index('dt_sessions_70k.csv'))
+    df = load_data(file_name)
+
+    res = analyze_paths(df)
+    fig = draw_page_tree(*res)
+    if st.button("Export page tree"):
+        export_page_tree(fig, file_name)
     # build_page_visit_graph(df)
 
 
--- src/utils/io.py
+++ src/utils/io.py
@@ -1,25 +1,34 @@
+import functools
 import os
 from pathlib import Path
-from typing import Callable
+from typing import Callable, List
 
 import pandas as pd
+
+
+_DATA_DIR = Path('./data/')
+
+
+def get_available_datasets() -> List[str]:
+    return os.listdir(_DATA_DIR / 'raw')
 
 
 def filter_dt_session(df: pd.DataFrame) -> pd.DataFrame:
     return df.loc[df.ua_type != "Xhr", df.columns != 'ua_type']
 
 
+@functools.lru_cache()
 def load_csv_data(file_name: str, prep_fn: Callable = filter_dt_session) -> pd.DataFrame:
     """
     Read csv file from directory.
     """
-    data_dir = Path('./data/')
-    filtered_file = data_dir / 'interim' / file_name
+    _DATA_DIR = Path('./data/')
+    filtered_file = _DATA_DIR / 'interim' / file_name
 
     if os.path.exists(filtered_file):
         return pd.read_csv(filtered_file)
     else:
-        df = pd.read_csv(data_dir / 'raw' / file_name)
+        df = pd.read_csv(_DATA_DIR / 'raw' / file_name)
         df_filtered = prep_fn(df)
         print(f"Loaded data: {len(df)} / after filtering {len(df_filtered)}")
         df_filtered.to_csv(filtered_file)  # store filtered DF for later re-usage
